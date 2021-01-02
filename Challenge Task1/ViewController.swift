a6da67edd31dc34c548a8dcb295c05a4893ffe4f
akio0911 <akio0911@gmail.com>
2021-01-02 22:29:16 +0900

    feedback

--- Challenge Task1/ViewController.swift
+++ Challenge Task1/ViewController.swift
@@ -7,47 +7,27 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
 
     @IBOutlet weak var textField1: UITextField!
-    
     @IBOutlet weak var textField2: UITextField!
-    
     @IBOutlet weak var textField3: UITextField!
-    
     @IBOutlet weak var textField4: UITextField!
-    
     @IBOutlet weak var textField5: UITextField!
     
     @IBOutlet weak var totalNumberLabel: UILabel!
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
     @IBAction func calculate(_ sender: Any) {
-        
-        
         // string?の型をInt型に変換
         
-        var number1 = Int(textField1.text ?? "") ?? 0
+        let number1 = Int(textField1.text ?? "") ?? 0
+        let number2 = Int(textField2.text ?? "") ?? 0
+        let number3 = Int(textField3.text ?? "") ?? 0
+        let number4 = Int(textField4.text ?? "") ?? 0
+        let number5 = Int(textField5.text ?? "") ?? 0
         
-        var number2 = Int(textField2.text ?? "") ?? 0
-        
-        var number3 = Int(textField3.text ?? "") ?? 0
-        
-        var number4 = Int(textField4.text ?? "") ?? 0
-        
-        var number5 = Int(textField5.text ?? "") ?? 0
-        
-        
-        var totalNumber = number1 + number2 + number3 + number4 + number5
+        let totalNumber = number1 + number2 + number3 + number4 + number5
         
         totalNumberLabel.text = "\(totalNumber)"
-        
-        
     }
-    
 }
-
